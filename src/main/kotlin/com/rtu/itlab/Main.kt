a80b587a4bba00465f249f4be2124f33778bb06c
Dmitry Romanov <battlebot98@gmail.com>
2018-11-13 00:14:47 +0300

    Fixed database reconnect and added logs

--- src/main/kotlin/com/rtu/itlab/Main.kt
+++ src/main/kotlin/com/rtu/itlab/Main.kt
@@ -3,17 +3,21 @@ package com.rtu.itlab
 import io.ktor.application.Application
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import org.slf4j.LoggerFactory
 
 //Server port
-private const val SPORTARG = "-sp"
-private var sPort = 80
+private const val SPORTARG = "-p"
+private var sPort = 8080
 
 fun main(args: Array<String>) {
+    val logger = LoggerFactory.getLogger("com.rtu.itlab.Server")
 
-    val portObtainedInArgs  = args.isNotEmpty() && args[0].startsWith(SPORTARG)
+    val portObtainedInArgs = args.isNotEmpty() && args[0].startsWith(SPORTARG)
 
-    if(portObtainedInArgs)
+    if (portObtainedInArgs)
         sPort = args[0].split("=").last().trim().toInt()
+
+    logger.info("Starting server at port $sPort")
 
     embeddedServer(Netty, sPort, module = Application::main).start(wait = true)
 }
