cd0f9238fe68efb8176568ee97e55019f5ea3f01
Dmitry Romanov <battlebot98@gmail.com>
2019-02-25 19:23:29 +0300

    Added key to community posts. Removed unnecessary alerts.

--- src/main/kotlin/com/rtu/itlab/Server.kt
+++ src/main/kotlin/com/rtu/itlab/Server.kt
@@ -41,149 +41,158 @@ fun Application.main() {
         post("/bot") {
             val tmp: JsonObject? =
                 Gson().fromJson(InputStreamReader(call.receiveStream(), "UTF-8"), JsonObject::class.java)
+            val config = Config().config!!
+            if (tmp!!.get("secret").asString == config.getString("group.secret")) {
+                if (!tmp.get("type").asString.matches(Regex("[0-9]*"))) {
 
-            if (!tmp!!.get("type").asString.matches(Regex("[0-9]*"))) {
-
-                when (tmp.get("type").asString) {
-                    "confirmation" -> {
-                        val config = Config().config!!
-                        call.respond(config.getString("server.response"))
-                    }
-                    "message_new" -> {
-                        VKMessageHandling(tmp, db).send()
-                        call.respond("OK")
-                    }
-                    "message_reply" -> {
-                        call.respond("OK")
-                    }
-                }
-
-            } else {
-                val type = tmp.get("type").asInt
-                if (type <= NotifyType.values().size - 1) {
-                    when (NotifyType.values()[tmp.get("type").asInt]) {
-
-                        NotifyType.EventNew -> {
-                            logger.info("Request for new event.")
-                            call.respond(EventNew(Gson().fromJson(tmp.get("data"), EventView::class.java), db).send())
+                    when (tmp.get("type").asString) {
+                        "confirmation" -> {
+                            val config = Config().config!!
+                            call.respond(config.getString("server.response"))
                         }
-
-                        NotifyType.EventChange -> {
-                            logger.info("Request for changed event")
-                            call.respond(
-                                EventChange(
-                                    Gson().fromJson(tmp.get("data"), EventView::class.java),
-                                    db
-                                ).send()
-                            )
+                        "message_new" -> {
+                            VKMessageHandling(tmp, db).send()
+                            call.respond("OK")
                         }
-
-                        NotifyType.EventConfirm -> {
-                            logger.info("Request for changed event")
-                            val userId = tmp.get("data").asJsonObject.get("user").asJsonObject.get("id").asString
-                            call.respond(
-                                EventConfirm(
-                                    Gson().fromJson(tmp.get("data"), EventView::class.java),
-                                    db,
-                                    userId
-                                ).send()
-                            )
+                        "message_reply" -> {
+                            call.respond("OK")
                         }
-
-                        else -> call.respondText { "Wrong type" }
                     }
+
                 } else {
-                    call.respond("Error number of NotifyType")
+                    val type = tmp.get("type").asInt
+                    if (type <= NotifyType.values().size - 1) {
+                        when (NotifyType.values()[tmp.get("type").asInt]) {
+
+                            NotifyType.EventNew -> {
+                                logger.info("Request for new event.")
+                                call.respond(
+                                    EventNew(
+                                        Gson().fromJson(tmp.get("data"), EventView::class.java),
+                                        db
+                                    ).send()
+                                )
+                            }
+
+                            NotifyType.EventChange -> {
+                                logger.info("Request for changed event")
+                                call.respond(
+                                    EventChange(
+                                        Gson().fromJson(tmp.get("data"), EventView::class.java),
+                                        db
+                                    ).send()
+                                )
+                            }
+
+                            NotifyType.EventConfirm -> {
+                                logger.info("Request for changed event")
+                                val userId = tmp.get("data").asJsonObject.get("user").asJsonObject.get("id").asString
+                                call.respond(
+                                    EventConfirm(
+                                        Gson().fromJson(tmp.get("data"), EventView::class.java),
+                                        db,
+                                        userId
+                                    ).send()
+                                )
+                            }
+
+                            else -> call.respondText { "Wrong type" }
+                        }
+                    } else {
+                        call.respond("Error number of NotifyType")
+                    }
                 }
+            } else {
+                call.respond("OK")
             }
         }
 
-        post("/bot/db") {
-            val tmp: JsonObject? =
-                Gson().fromJson(InputStreamReader(call.receiveStream(), "UTF-8"), JsonObject::class.java)
-
-            when (tmp!!.get("type").asString) {
-
-                "disconnect" -> {
-                    db.closeConnection()
-                    val result = JsonObject()
-                    result.addProperty("statusCode", 1)
-                    logger.info("Disconnecting from database")
-                    call.respond(result)
-                }
-
-                "addPerson" -> {
-                    call.respond(db.addPerson(tmp.getAsJsonObject("data")))
-                }
-
-                "personUpdate" -> {
-                    call.respond(db.updatePersonInfo(tmp.getAsJsonObject("data")))
-                }
-
-                "addPersons" -> {
-                    logger.info("Request for adding person.")
-                    call.respond(
-                        db.addPersons(
-                            Gson().fromJson(
-                                tmp.getAsJsonArray("data"),
-                                Array<DBUser>::class.java
-                            )
-                        )
-                    )
-                }
-
-            }
-        }
-
-        get("bot/db/persons/ispersonindbbyvkid/{vkid}") {
-            call.respond(db.isUserInDBByVkId(call.parameters["vkid"]!!.toInt()))
-        }
-
-        get("/bot/db/persons/get") {
-            call.respond(db.getAllPersons())
-        }
-
-        get("/bot/db/persons/mailnotice") {
-            call.respond(db.getUsersMailsForEmailMailing())
-        }
-
-        get("/bot/db/persons/phonenotice") {
-            call.respond(db.getUsersPhonesForPhoneMailing())
-        }
-
-        get("/bot/db/persons/vknotice") {
-            call.respond(db.getUsersVkIdForVkMailing())
-        }
-
-        get("/bot/db/person/{id}") {
-            call.respond(db.getUserInfoByKey(call.parameters["id"]))
-        }
-
-        get("/bot/db/dump") {
-            call.respond(db.makeDump())
-        }
-
-        get("/bot/keyboard/{vkId}") {
-            call.respond(getKeyboardForCurrentPerson(call.parameters["vkId"]!!.toInt(), db).getKeyboardJson())
-        }
-
-        delete("/bot/db/person/delete/{id}") {
-            call.respond(db.deletePerson(call.parameters["id"]))
-        }
-
-        delete("/bot/db/persons/delete") {
-            call.respond(db.deleteAllPersons())
-        }
-
-        get("/bot/db/connect") {
-            call.respond(db.loadConfigAndConnect())
-        }
-
-        get("/bot/db/isconnected") {
-            val result = JsonObject()
-            result.addProperty("connection", db.isConnected())
-            call.respond(result)
-        }
+//        post("/bot/db") {
+//            val tmp: JsonObject? =
+//                Gson().fromJson(InputStreamReader(call.receiveStream(), "UTF-8"), JsonObject::class.java)
+//
+//            when (tmp!!.get("type").asString) {
+//
+//                "disconnect" -> {
+//                    db.closeConnection()
+//                    val result = JsonObject()
+//                    result.addProperty("statusCode", 1)
+//                    logger.info("Disconnecting from database")
+//                    call.respond(result)
+//                }
+//
+//                "addPerson" -> {
+//                    call.respond(db.addPerson(tmp.getAsJsonObject("data")))
+//                }
+//
+//                "personUpdate" -> {
+//                    call.respond(db.updatePersonInfo(tmp.getAsJsonObject("data")))
+//                }
+//
+//                "addPersons" -> {
+//                    logger.info("Request for adding person.")
+//                    call.respond(
+//                        db.addPersons(
+//                            Gson().fromJson(
+//                                tmp.getAsJsonArray("data"),
+//                                Array<DBUser>::class.java
+//                            )
+//                        )
+//                    )
+//                }
+//
+//            }
+//        }
+//
+//        get("bot/db/persons/ispersonindbbyvkid/{vkid}") {
+//            call.respond(db.isUserInDBByVkId(call.parameters["vkid"]!!.toInt()))
+//        }
+//
+//        get("/bot/db/persons/get") {
+//            call.respond(db.getAllPersons())
+//        }
+//
+//        get("/bot/db/persons/mailnotice") {
+//            call.respond(db.getUsersMailsForEmailMailing())
+//        }
+//
+//        get("/bot/db/persons/phonenotice") {
+//            call.respond(db.getUsersPhonesForPhoneMailing())
+//        }
+//
+//        get("/bot/db/persons/vknotice") {
+//            call.respond(db.getUsersVkIdForVkMailing())
+//        }
+//
+//        get("/bot/db/person/{id}") {
+//            call.respond(db.getUserInfoByKey(call.parameters["id"]))
+//        }
+//
+//        get("/bot/db/dump") {
+//            call.respond(db.makeDump())
+//        }
+//
+//        get("/bot/keyboard/{vkId}") {
+//            call.respond(getKeyboardForCurrentPerson(call.parameters["vkId"]!!.toInt(), db).getKeyboardJson())
+//        }
+//
+//        delete("/bot/db/person/delete/{id}") {
+//            call.respond(db.deletePerson(call.parameters["id"]))
+//        }
+//
+//        delete("/bot/db/persons/delete") {
+//            call.respond(db.deleteAllPersons())
+//        }
+//
+//        get("/bot/db/connect") {
+//            call.respond(db.loadConfigAndConnect())
+//        }
+//
+//        get("/bot/db/isconnected") {
+//            val result = JsonObject()
+//            result.addProperty("connection", db.isConnected())
+//            call.respond(result)
+//        }
 
     }
 }
--- src/main/kotlin/com/rtu/itlab/responses/VKMessageHandling.kt
+++ src/main/kotlin/com/rtu/itlab/responses/VKMessageHandling.kt
@@ -72,12 +72,14 @@ class VKMessageHandling(tmp: JsonObject?, db: DBClient) : ResponseHandler(db) {
     private var keyboard = "{\"buttons\":[],\"one_time\":true}"
 
     private fun sendMessage(message: String) {
-        vk.messages()
-            .send(actor)
-            .userId(vkId)
-            .message(message)
-            .keyboard(keyboard)
-            .execute()
+        if (message.isNotEmpty()) {
+            vk.messages()
+                .send(actor)
+                .userId(vkId)
+                .message(message)
+                .keyboard(keyboard)
+                .execute()
+        }
     }
 
     private fun getKeyboardJson(): String {
@@ -90,7 +92,7 @@ class VKMessageHandling(tmp: JsonObject?, db: DBClient) : ResponseHandler(db) {
 
 
     override fun send(): JsonObject {
-        val message: String
+        var message = ""
 
         if (db!!.isUserInDBByVkId(vkId).get("result").asString != "true") {
 
@@ -114,56 +116,37 @@ class VKMessageHandling(tmp: JsonObject?, db: DBClient) : ResponseHandler(db) {
                 }
 
                 message = if (responseObject != null) {
-                    when (responseObject.statusCode) {
-                        1 -> {
-                            //Getting result of adding person to database
-                            val addingResult = db.addPerson(
-                                responseObject.data!!.copy(
-                                    vkId = vkId.toString(), vkNotice = true,
-                                    emailNotice = true, phoneNotice = true
-                                )
-                            ).get("statusCode").asInt
+                    if (responseObject.statusCode == 1) {
+
+                        //Getting result of adding person to database
+                        val addingResult = db.addPerson(
+                            responseObject.data!!.copy(
+                                vkId = vkId.toString(), vkNotice = true,
+                                emailNotice = true, phoneNotice = true
+                            )
+                        ).get("statusCode").asInt
 
 
-                            //If person added then 1, else send message to user with error 1
-                            when (addingResult) {
-                                1 -> {
-                                    keyboard = getKeyboardJson()
-                                    GlobalScope.launch { sendEmail() }
-                                    "Поздравляем, ваша учетня запись прикреплена"
-                                }
-                                else -> "При добавлении вашей учетной записи произошла ошибка 1"
+                        //If person added then 1, else send message to user with error 1
+                        when (addingResult) {
+                            1 -> {
+                                keyboard = getKeyboardJson()
+                                GlobalScope.launch { sendEmail() }
+                                "Поздравляем, ваша учетня запись прикреплена"
                             }
-
+                            else -> "При добавлении вашей учетной записи произошла ошибка 1"
                         }
-
-                        //If the auth code (token) was entered incorrectly
-                        26 -> "Проверьте правильность написания кода"
-
-                        //If there are any other errors
-                        else -> "При добавлении вашей учетной записи произошла ошибка 2"
+                    } else {
+                        ""
                     }
                 } else {
-                    "При добавлении вашей учетной записи произошла ошибка 3"
+                    ""
                 }
-            } else {
-                //if the user has sent a non-template code message
-                message = "Если вы пытались прислать код для верификация, то вы сделали это как-то не так\n" +
-                        "Я не веду бесед с незнакомцами\n" +
-                        "Проверьте правильность написания кода"
-            }
-        } else if (db.isUserInDBByVkId(vkId).get("result").asString == "unknown") {
 
-            message = when (messageText.startsWith("L:")) {
-                true -> "Простите, на данный момент мы не можем вас авторизовать!\n" +
-                        "Повторите вашу попытку позже"
-                else -> "Я не понимаю, что вы хотели мне сказать."
             }
-
         } else {
             //If user already authorized in the system
             if (messageText.startsWith("L:")) {
-
                 message = "Ранее вы уже были авторизованы!"
             } else {
                 message = when (BotCommands.getEnumClassByCommandText(messageText)) {
