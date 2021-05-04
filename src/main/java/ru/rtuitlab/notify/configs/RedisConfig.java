46987849f7f490dc44731066be3159e149b7ff1c
redbird <redbird8941@gmail.com>
2021-05-04 22:24:51 +0300

    Divide configs

---
+++ src/main/java/ru/rtuitlab/notify/configs/RedisConfig.java
@@ -0,0 +1,11 @@
+//package ru.rtuitlab.notify.configs;
+//
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.stereotype.Component;
+//import org.springframework.validation.annotation.Validated;
+//
+//@Component
+//@Validated
+//@PropertySource(value = "")
+//public class RedisConfig {
+//}
